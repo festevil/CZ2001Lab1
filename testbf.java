f3d154b6a5c696597105b8b89d45e51cc9284d8a
festevil <62298846+festevil@users.noreply.github.com>
2020-09-16 13:25:43 +0800

    Add files via upload

--- testbf.java
+++ testbf.java
@@ -1,19 +1,26 @@
 package com.algo;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class testbf {
 	public static void main(String[] args)
 	{
 		String data = "";
+		Scanner Scan = new Scanner(System.in);
 		try
 		{	// extract a txt file as a variable, file has to be in the same directory
-			data = new String(Files.readAllBytes(Paths.get("C:/Users/Benjamin Tan/eclipse-workspace/AlgorithmLab1/src/com/algo/GCF_000006945.2_ASM694v2_genomic.fna"))); 		}
+			System.out.println("Input directory of file...");
+			String path = Scan.nextLine();
+			data = new String(Files.readAllBytes(Paths.get(path))); 		}
 		catch (IOException e) {
 		      e.printStackTrace();
 		    }
-		String string = "CCTCCCT";
+		try {
+		System.out.println("Enter pattern to search for...");
+		String string = Scan.next();
 		System.out.println("Brute Force");
 		BruteForce.Brute(string,data);
 		System.out.println();
@@ -22,6 +29,13 @@ public class testbf {
 		System.out.println();
 		System.out.println("Boyer Moore");
 		BoyerMoore.search(string.toCharArray(), data.toCharArray());
+		}
+		catch(Exception e){
+			System.out.println("Something went wrong.");
+		}
+		finally {
+			Scan.close();
+		}
 		return;
 	}
 }
