f8cbb25bb5607c96ecd12434956b8ff422ce49cf
festevil <62298846+festevil@users.noreply.github.com>
2020-09-21 10:56:45 +0800

    Add files via upload

--- BruteForce.java
+++ BruteForce.java
@@ -4,10 +4,12 @@ public class BruteForce{
 	public static void Brute(String inputstring, String datastring) {
 		int inputlength = inputstring.length();
 		int datalength = datastring.length();
+		boolean exist = false;
 		for (int i = 0; i < (datalength-inputlength+1); i++) {
 			boolean a = true;
 			for (int k = 0; k < inputlength; k++) {
 				if (inputstring.charAt(k) != datastring.charAt(i + k)) {
+					exist = true;
 					a = false;
 					break;
 				}
@@ -15,6 +17,10 @@ public class BruteForce{
 			if (a == true) {
 				System.out.print(" " + (i + 1) + " ");
 			}
+			
+		}
+		if(exist) {
+			System.out.print("-1");
 		}
 	}
 
